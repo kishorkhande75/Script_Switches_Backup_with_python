eda3e85a57acd3f2c8b3c20193745be0298b40cf
Kishor Khande <kishorkhande75@gmail.com>
2023-01-05 11:21:28 +0530

    first commit

--- Email.py
+++ Email.py
@@ -7,8 +7,8 @@ from email import encoders
 
 def Email():
     
-    fromaddr = "do-not-reply@forgeahead.io" #EMAIL address of the sender
-    toaddr = "si@forgeahead.io" #EMAIL address of the receiver
+    fromaddr = "XYZ" #EMAIL address of the sender
+    toaddr = "ZYZ" #EMAIL address of the receiver
 
     # instance of MIMEMultipart
     msg = MIMEMultipart()
@@ -23,7 +23,7 @@ def Email():
     msg['Subject'] = "SI-Alert: Automated Network Devices Backup Script Executed Successfully!!" # Subject of the Mail
 
     # string to store the body of the mail
-    body = "Hello Team, \n \nAutomated script executed successfully to take the configuration backup of network devices listed below:- \n \n 1. Core_Switch \n 2. Switch1 \n 3. Switch2 \n \nThank you! \n_ _ \nRegards, \nKishor Khande.\n--------------------------------------------------------------------------------------------------------\nThis is a system generated alert. Do not reply to this email. \n--------------------------------------------------------------------------------------------------------" # Body_of_the_mail
+    body = "Hello Team, \n \nAutomated script executed successfully to take the configuration backup of network devices listed below:- \n \n 2. Switch1 \n 3. Switch2 \n \nThank you! \n_ _ \nRegards, \nKishor Khande.\n--------------------------------------------------------------------------------------------------------\nThis is a system generated alert. Do not reply to this email. \n--------------------------------------------------------------------------------------------------------" # Body_of_the_mail
 
     # attach the body with the msg instance
     msg.attach(MIMEText(body, 'plain'))
@@ -53,7 +53,7 @@ def Email():
     s.starttls()
 
     # Authentication
-    s.login(fromaddr, "y5qaMXG2VN86aW0u") #Password_of_the_sender
+    s.login(fromaddr, "ABC") #Password_of_the_sender
 
     # Converts the Multipart msg into a string
     text = msg.as_string()
--- Switches.py
+++ Switches.py
@@ -7,7 +7,7 @@ import datetime
 import json
 import Email
 
-device_list = 'C:\\Users\\si\\Documents\\Switch\\Switches.json'
+device_list = 'C:\\Users\\ab\\Documents\\Switch\\Switches.json'
 backup_filename = 'SW-Config-Backup-' + '{0:%Y-%m-%d-%H-%M-%S}'.format(datetime.datetime.now()) + '.cfg'
 # vlan_filename = 'RTR-Show-VLAN-' + '{0:%Y-%m-%d-%H-%M-%S}'.format(datetime.datetime.now()) + '.txt'
 
@@ -18,9 +18,9 @@ with open(device_list) as json_file:
         cisco_2960 = {
     	    'device_type': 'cisco_ios',
     	    'host':   switch['ip'],
-    	    'username': 'kishor',    # Provide SSH username
-    	    'password': 'Forge@2019$',    # Provide SSH password
-    	    'secret': 'Forge@2018',     # Optional, defaults to ''
+    	    'username': 'abc',    # Provide SSH username
+    	    'password': 'XYZ',    # Provide SSH password
+    	    'secret': 'XYZ',     # Optional, defaults to ''
 	}
 
         try:
@@ -37,7 +37,7 @@ with open(device_list) as json_file:
         net_connect.disconnect()
 
         #Create a separate directory for each device if not exists.
-        backup_dir = 'C:\\Users\\si\Documents\\Backup\\'+switch['hostname']
+        backup_dir = 'C:\\Users\\ab\Documents\\Backup\\'+switch['hostname']
         if not os.path.exists(backup_dir):
             os.makedirs(backup_dir)
 
